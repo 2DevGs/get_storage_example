1d7262f3da5425cca189339fcf804b9a5f915e75
2DevGs <dev.gustavoserrano@gmail.com>
2025-08-11 16:13:48 -0300

    Basic (Startup, Writing and Reading)

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var nome = GetStorage().read('nameKey') ?? '';
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(nome),
+            ElevatedButton(
+              onPressed: () {
+                GetStorage().write('nameKey', 'Gustavo Dias');
+                setState(() {
+                  
+                });
+              },
+              child: const Text('Gravar nome'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
